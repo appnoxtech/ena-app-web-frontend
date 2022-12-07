24776c3f2a8947672007001c2f172b0e48aa1dc3
HIMANSHU LODHI <HIMANSHU.LODHI.ALIGARH@GMAIL.COM>
2022-12-07 13:43:11 +0530

    checkout in progress

--- src/component/Common/addAddress/AddAddress.tsx
+++ src/component/Common/addAddress/AddAddress.tsx
@@ -9,12 +9,58 @@ function AddAddress() {
         {
             lable:'Name',
             type:'text',
-            id:'username'
+            id:'username',
+            name:'username',
+            placeholder:'Enter text here'
         },
         {
             lable:'Phone Number',
             type:'number',
-            id:''
+            id:'phone',
+            name:'phone',
+            placeholder:'Enter text here'
+        },
+    ]
+    const shippingAddress = [
+        {
+            lable:'Country',
+            type:'text',
+            id:'country',
+            name:'country',
+            placeholder:'Enter text here'
+        },
+        {
+            lable:'State',
+            type:'text',
+            id:'state',
+            name:'state',
+            placeholder:'Enter text here'
+        },
+        {
+            lable:'City',
+            type:'text',
+            id:'city',
+            name:'city',
+            placeholder:'Enter text here'
+        },
+        {
+            lable:'Pincode',
+            type:'number',
+            id:'pincode',
+            name:'pincode',
+            placeholder:'Enter text here'
+        },
+        {
+            lable:'Locality / Area / Street',
+            type:'text',
+            id:'address',
+            placeholder:'Enter text here'
+        },
+        {
+            lable:'Flat No. / Building Name',
+            type:'text',
+            id:'address',
+            placeholder:'Enter text here'
         },
     ]
   return (
@@ -26,8 +72,25 @@ function AddAddress() {
                 </div>
                 <div className="col-12">
                     {
-                        
-                        
+                        contactInfoItem.map((item)=>(
+                            <>
+                                <label className='form-label mt-3 h6 d-none d-lg-block d-md-block'>Email</label>
+                <LoginInput
+                  type={item.type}
+                  name={item.name}
+                  id={item.id}
+                  placeholder={item.placeholder}
+                  class='form-control mt-3 d-none d-md-block d-lg-block'
+                />
+                <LoginInput
+                  type={item.type}
+                  name={item.name}
+                  id={item.id}
+                  placeholder={item.placeholder}
+                  class='form-control mt-3 d-md-none d-lg-none d-sm-block'
+                />
+                            </>
+                        ))
                     }
                 </div>
             </div>
