d16732af669380bb9b38b69f8722e60442064219
Akalax <sahakash7512.com>
2022-12-09 21:42:56 +0530

    code in progress

---
+++ src/component/button/Button.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Button = (props: any) => {
+  console.log(props.props, 'props')
+  return (
+    <div>
+      <button type='button' className={`btn btn-outline-${props.props.styleName}  `}>
+        {props.props.indexData}
+      </button>
+    </div>
+  )
+}
+
+export default Button
+//<button shjfsgwg hudwjsg/>
--- src/component/order/Order.tsx
+++ src/component/order/Order.tsx
@@ -1,70 +1,155 @@
 import React from 'react'
 import Table from 'react-bootstrap/Table'
-import Tomato from '../../assets/images/6-tomato-png-image.png'
-
+import Tomato from '../../assets/images/carrot.jpg'
+import DynamicButton from '../button/Button'
 import './Order.css'
 const Order = () => {
   return (
     <div className='container-fluid pb-5'>
-      <div className='side-Part rounded-4 bg-white'></div>
-      <div className='col-10 mx-auto'>
-        <Table responsive className='orders_Heading rounded'>
-          <thead>
-            <tr>
-              <th></th>
-              <th>Product</th>
-              <th>Price</th>
-              <th>Size</th>
-              <th>Subtotal</th>
-              <th>Status</th>
-              <th>Action</th>
-            </tr>
-          </thead>
-          <tbody className='shopingCartTableBody'>
-            {Array.from({ length: 2 }).map((data, index) => (
-              <tr key={index}>
-                <td>
-                  <div className=''>
-                    <img src={Tomato} alt='img' className='img-fluid order_image' />
-                  </div>
-                </td>
-                <td>
-                  <h5>Carrot</h5>
-                  <p className='order_Id'>Order id:1234</p>
-                </td>
-                <td>
-                  <h6>kn 35.2/kg</h6>
-                </td>
-                <td>
-                  <div className=''>
-                    <h5>10 kg</h5>
-                  </div>
-                </td>
-                <td>
-                  <h5>kn 35560</h5>
-                </td>
-                <td>
-                  {index  == 0 ? (
-                    <div className=''>
-                      <h5>Approved</h5>
-                    </div>
-                  ) : (
-                    <div className=''>
-                      <h5>Pending</h5>
-                    </div>
-                  )}
-                </td>
-                <td>
-                  <div className=''>
-                    <h5>Remove</h5>
-                  </div>
-                </td>
+      <div className='side-Part rounded-4 bg-white'>
+        <div className='col-12 mx-auto'>
+          <Table responsive className='orders_Heading rounded table-hover'>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Size</th>
+                <th>Subtotal</th>
+                <th>Status</th>
+                <th>Action</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
+            </thead>
+            <tbody className='shopingCartTableBody'>
+              {Array.from({ length: 2 }).map((data, index) => (
+                <tr key={index}>
+                  <td>
+                    <div className=''>
+                      <img src={Tomato} alt='img' className='img-fluid order_image' />
+                    </div>
+                  </td>
+                  <td>
+                    <h5>Carrot</h5>
+                    <p className='order_Id'>Order id:1234</p>
+                  </td>
+                  <td>
+                    <h6>kn 35.2/kg</h6>
+                  </td>
+                  <td>
+                    <div className=''>
+                      <h5>10 kg</h5>
+                    </div>
+                  </td>
+                  <td>
+                    <h5 className='fw-bold'>kn 35560</h5>
+                  </td>
+                  <td>
+                    {index == 0 ? (
+                      <p className='approved_text'>
+                        Approved
+                        {/* <DynamicButton/> */}
+                      </p>
+                    ) : (
+                      <p className='pending_text'>Pending</p>
+                    )}
+                  </td>
+                  <td>
+                    {index == 0 ? (
+                      <div className='d- flex align-items-center justify-content-between'>
+                        <DynamicButton
+                          props={{ styleName: 'dark px-5  p-0', indexData: 'Track' }}
+                        />
+
+                        <DynamicButton
+                          props={{ styleName: 'danger px-3 p-0 mt-2', indexData: 'Cancel Order' }}
+                        />
+                      </div>
+                    ) : (
+                      <div className=''>
+                        <DynamicButton
+                          props={{ styleName: 'danger py-1 p-0 px-3', indexData: 'Cancel Order' }}
+                        />
+                      </div>
+                    )}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
       </div>
     </div>
+
+    // <table className='table  table-hover '>
+    //   <thead>
+    //     <tr className="border">
+    //       <th></th>
+    //       <th scope='col' className='text-center'>
+    //         Product
+    //       </th>
+    //       <th scope='col' className='text-center'>
+    //         Price
+    //       </th>
+    //       <th scope='col' className='text-center'>
+    //         Size
+    //       </th>
+    //       <th scope='col' className='text-center'>
+    //         Subtotal
+    //       </th>
+    //       <th scope='col' className='text-center'>
+    //         Status
+    //       </th>
+    //       <th scope='col' className='text-center'>
+    //         Action
+    //       </th>
+    //     </tr>
+    //   </thead>
+    //   <tbody className='shopingCartTableBody'>
+    //     {Array.from({ length: 2 }).map((data, index) => (
+    //       <tr key={index}>
+    //         {/* <td className='text-center'>
+    //           <div className=' d-flex align-i'>
+    //             <img src={Tomato} alt='img' className='img-fluid order_image' />
+    //             <h5>Carrot</h5>
+    //           <p className='order_Id'>Order id:1234</p>
+    //           </div>
+    //         </td> */}
+    //         <td>
+    //           <div className=''>
+    //             <img src={Tomato} alt='img' className='img-fluid order_image' />
+    //           </div>
+    //         </td>
+    //         <td>
+    //           <h5>Carrot</h5>
+    //           <p className='order_Id'>Order id:1234</p>
+    //         </td>
+    //         <td>
+    //           <h6>kn 35.2/kg</h6>
+    //         </td>
+    //         <td>
+    //           <div className=''>
+    //             <h5>10 kg</h5>
+    //           </div>
+    //         </td>
+    //         <td>
+    //           <h5 className='fw-bold'>kn 35560</h5>
+    //         </td>
+    //         <td>
+    //           {index == 0 ? (
+    //             <p className='approved_text'>Approved</p>
+    //           ) : (
+    //             <p className='pending_text'>Pending</p>
+    //           )}
+    //         </td>
+    //         <td>
+    //           <div className=''>
+    //             <h5>Remove</h5>
+    //           </div>
+    //         </td>
+    //       </tr>
+    //     ))}
+    //   </tbody>
+    // </table>
   )
 }
 
