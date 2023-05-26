fd56212a1036b0f1991a8c84f7a163243d25e08a
Shudhanshu-Appnox <shudhanshu.choudhary@appnox.ai>
2023-05-26 18:59:40 +0530

    change the backend url

--- src/GlobalVariable.tsx
+++ src/GlobalVariable.tsx
@@ -1,8 +1,3 @@
-// let hostname = process.env.REACT_APP_DEV_URL
-
-// console.log('hostname', hostname);
-
-//export const hostname = 'http://172.16.2.152:5000';
-export const hostname = 'https://backend.appnox.website';
+export const hostname = 'https://enabackend.appnox.website';
 
 export const API_URL = `${hostname}/api`;
