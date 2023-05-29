06cc391a3e5882f4c26e6b8eefa5f87eb7c113b3
Shudhanshu-Appnox <shudhanshu.choudhary@appnox.ai>
2023-05-29 16:09:23 +0530

    change live url

--- src/GlobalVariable.tsx
+++ src/GlobalVariable.tsx
@@ -3,6 +3,6 @@
 // console.log('hostname', hostname);
 
 //export const hostname = 'http://172.16.2.152:5000';
-export const hostname = 'https://backend.appnox.website';
+export const hostname = 'https://enabackend.appnox.website';
 
 export const API_URL = `${hostname}/api`;
